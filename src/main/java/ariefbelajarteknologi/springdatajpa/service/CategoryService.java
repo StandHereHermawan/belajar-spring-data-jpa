4f4679eab25588b028d05aba07496f53623856a3
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-14 03:27:55 +0700

    Programmatic Transaction, add service class

--- src/main/java/ariefbelajarteknologi/springdatajpa/service/CategoryService.java
+++ src/main/java/ariefbelajarteknologi/springdatajpa/service/CategoryService.java
@@ -4,7 +4,12 @@ import ariefbelajarteknologi.springdatajpa.entity.Category;
 import ariefbelajarteknologi.springdatajpa.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionOperations;
 
 @Service
 public class CategoryService {
@@ -12,11 +17,53 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private TransactionOperations transactionOperations;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    public void manual() {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setTimeout(10);
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        TransactionStatus transaction = transactionManager.getTransaction(definition);
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category " + i);
+                categoryRepository.save(category);
+            }
+            error();
+            transactionManager.commit(transaction);
+        } catch (Throwable throwable) {
+            transactionManager.rollback(transaction);
+            throw throwable;
+        }
+    }
+
+    public void error() {
+        throw new RuntimeException("Ups");
+    }
+
+    public void createCategoriesError() {
+        transactionOperations.executeWithoutResult(transactionStatus -> {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category " + i);
+                categoryRepository.save(category);
+            }
+            error();
+        });
+    }
+
     @Transactional
-    public void create(){
+    public void create() {
         for (int i = 0; i < 5; i++) {
             Category category = new Category();
-            category.setName("Category "+i);
+            category.setName("Category " + i);
             categoryRepository.save(category);
         }
         throw new RuntimeException("Ups Rollback please");
