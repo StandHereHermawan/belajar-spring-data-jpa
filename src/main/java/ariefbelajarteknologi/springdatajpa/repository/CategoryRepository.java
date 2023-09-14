2498cadf9cab5ba0c89adbe57c8a65ebd64cb21d
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-15 01:54:15 +0700

    Query Method, modify repository interface

--- src/main/java/ariefbelajarteknologi/springdatajpa/repository/CategoryRepository.java
+++ src/main/java/ariefbelajarteknologi/springdatajpa/repository/CategoryRepository.java
@@ -4,7 +4,16 @@ import ariefbelajarteknologi.springdatajpa.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    // where name = ?
+    Optional<Category> findFirstByNameEquals(String name);
+
+    // where name like = ?
+    List<Category> findAllByNameLike(String name);
 
 }
