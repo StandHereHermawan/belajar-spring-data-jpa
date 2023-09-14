d8349be02f7a0fc564536ef66c796098f0c05ac0
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-15 02:35:27 +0700

    Query Relation, add entity class

---
+++ src/main/java/ariefbelajarteknologi/springdatajpa/entity/Product.java
@@ -0,0 +1,28 @@
+package ariefbelajarteknologi.springdatajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",referencedColumnName = "id")
+    private Category category;
+}
