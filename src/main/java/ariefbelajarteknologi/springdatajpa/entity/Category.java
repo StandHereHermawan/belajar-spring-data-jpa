a7454070c93ffa43ec8d818793ea344edc87ae85
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-15 02:36:16 +0700

    Query Relation, modify entity class

--- src/main/java/ariefbelajarteknologi/springdatajpa/entity/Category.java
+++ src/main/java/ariefbelajarteknologi/springdatajpa/entity/Category.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -19,4 +21,7 @@ public class Category {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
