5c195cc2ea176f091b50d92e6edb96b24d881493
StandHereHermawan <nolifegansaya@gmail.com>
2023-09-16 03:43:32 +0700

    Pageable, modify repository interface

--- src/main/java/ariefbelajarteknologi/springdatajpa/repository/ProductRepository.java
+++ src/main/java/ariefbelajarteknologi/springdatajpa/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package ariefbelajarteknologi.springdatajpa.repository;
 
 import ariefbelajarteknologi.springdatajpa.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory_Name(String name, Sort sort);
 
+    List<Product> findAllByCategory_Name(String name, Pageable pageable);
 }
